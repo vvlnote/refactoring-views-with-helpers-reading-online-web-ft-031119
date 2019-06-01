34bbba99cc3fd1dff62ccfc2b66650aa511d7152
Joy Lin <vvlnote@hotmail.com>
2019-06-01 23:24:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,6 @@ class PostsController < ApplicationController
 	private
 
 		def post_params
-			params.require(:post).premit(:title, :description)
+			params.require(:post).permit(:title, :description)
 		end
 end
